fafd2723beca5c301b7ef97a10d1b94d88f66d9c
Tara1008 <rima282017@gmail.com>
2025-08-08 14:24:37 +0530

    path_changed

--- Assignment-1/test.py
+++ Assignment-1/test.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.pt")
 
 # Predict on your local image
-results = model("D:\\IIITH_internship\\Assignment_1\\bus.jpg")
+results = model("path/to/your/image")
 
 # Show results
 results[0].show()       # Optional: Display window with results
